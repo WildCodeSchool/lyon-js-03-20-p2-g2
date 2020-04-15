3c4165c778ceaba567341c689e58aadee26442df
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-04-15 12:39:57 +0200

    starts the modal suggestions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import ModalSuggestions from './components/ModalSuggestions';
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <div id='App'>
         <Header />
         <SearchBar />
+        <ModalSuggestions />
       </div>
     );
   }
