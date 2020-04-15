410aa9529f065c7e5ddd1042eba7d424783a74ca
abdel <a.akroh@e-durable.ch>
2020-04-15 14:10:59 +0200

    #03 function handle

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,40 @@ import '../style/search-bar.css';
 
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+      results: {},
+      loading: false,
+      message: '',
+    };
+  }
+
+  //***handleOnInputChange arrow FUNCTION expression *****/
+
+  handleOnInputChange = (event) => {
+    const query = event.target.value;//get the input value
+    this.setState({ query, loading: true, message: '' });//replace by the current state
+    console.log(query)
+  };
+
+
   render() {
     return (
       <form className="search-bar">
-        <input type="text" onChange={this.props.search} name="search" placeholder="Search City" />
+        <label className="search-label" htmlFor="search-input">
+
+          <input
+            type="text"
+            value=""
+            id="search-input"
+            placeholder="Search City..."
+            onChange={this.handleOnInputChange}
+          />
+
+        </label>
       </form>
     )
   }
