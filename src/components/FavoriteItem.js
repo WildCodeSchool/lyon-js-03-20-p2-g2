0dcdca0751bba9d09e8735cfa7e45e6efa8ce580
Sora952 <sora952c16b10@gmail.com>
2020-04-21 00:47:01 +0200

    #13 Divers patch de création

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -2,24 +2,13 @@ import React from 'react';
 import '../style/favorite-item.css';
 
 class FavoriteItem extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      favorite: true
-    };
-  }
-
   render () {
     return (
       <div>
 
         <span
-          className={this.state.favorite ? 'is-favorite' : 'empty'}
-          onClick={event => {
-            const newFavorite = !this.state.favorite;
-
-            this.setState({ favorite: newFavorite });
-          }}
+          className={this.props.favorite ? 'is-favorite' : 'empty'}
+          onClick={this.props.handleChange}
         >
           <i className='fas fa-heart' />
 
--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../style/favorites.css';
-
-const cities = [];
+import { FavItem } from './FavoriteItem';
 
 class Favorites extends Component {
   constructor (props) {
@@ -11,12 +10,46 @@ class Favorites extends Component {
       list: [],
       favList: []
     };
-    this.list = [];
+    this.list = [(
+      Lyon: 
+        id: 1, 
+        favorite: false,
+      Paris: 
+        id: 2, 
+        favorite: false, 
+      Maison: 
+        id: 3, 
+        favorite: false
+    )];
   }
+
+  addToFav = cityId => {
+    this.setState(
+      prevstate => {
+        return {
+          favList: [...prevstate.favList, cityId]
+        };
+      },
+      () => {}
+    );
+  };
+
+  handleChange = (city) => {
+    const newFavorite = !this.state.city.favorite;
+
+    this.setState({ favorite: newFavorite });
+  };
 
   render () {
     return (
-      <div className='favorites' />
+      <div className='favorites'>
+        <ul>
+          {this.state.list.map(city => {
+            return (<li key={city}>{city}</li>);
+          })}
+        </ul>
+        <FavItem onClick={this.handleChange} active={this.state.city.favorite} />
+      </div>
     );
   }
 }
