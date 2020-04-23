825e0d44fd4ef0d18f5c08925b09b2d68f310cd2
wdwcs <Arminit3@gmail.com>
2020-04-23 16:39:52 +0200

    #6 add background function changing according to time

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar';
 import ModalSuggestions from '../components/ModalSuggestions';
 import '../style/home.css';
 import Header from '../components/Header';
 
-const Home = (props) => {
-  return (
-    <div className='home'>
-      <Header />
-      <SearchBar onClick={props.onClick} />
-      <ModalSuggestions />
-    </div>
-  );
-};
+let className = '';
+
+class Home extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      weatherClassName: 'day'
+    };
+  }
+
+  componentWillMount () {
+    this.getWeatherClassName();
+  }
+
+  getWeatherClassName () {
+    var today = new Date();
+    var time = today.getHours();
+    //var time = 1;
+
+    if (time >= 6 && time < 12) {
+      className = 'aube';
+    } else if (time >= 12 && time < 18) {
+      className = 'day';
+    } else if (time >= 18 && time <= 23) {
+      className = 'dusk';
+    } else if (time >= 0 && time < 6) {
+      className = 'night';
+    }
+    this.setState({
+      weatherClassName: className
+    });
+  }
+
+  render () {
+    return (
+      <div className={this.state.weatherClassName}>
+        <Header />
+        <ModalSuggestions />
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 export default Home;
