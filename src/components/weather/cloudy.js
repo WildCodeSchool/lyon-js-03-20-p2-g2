b066d9f3e6513862aefa7205852daac7ccd28c54
Sora952 <sora952c16b10@gmail.com>
2020-04-26 00:29:58 +0200

    #10 Adding cards grid

--- src/components/weather/cloudy.js
+++ src/components/weather/cloudy.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Grid } from 'semantic-ui-react';
+import _ from 'lodash';
 import '../../style/cardsWeatherWears.css';
+
+const columns = _.times(5, (i) => (
+  <Grid.Column key={i}>
+    <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+  </Grid.Column>
+));
+
+const GridExampleGrid = () => <Grid>{columns}</Grid>;
 
 class Cloudy extends Component {
   render () {
@@ -9,12 +18,12 @@ class Cloudy extends Component {
         <Card.Content>
 
           <Card.Header>Our suggestion</Card.Header>
-          <Image floated='right' size='tiny' src='https://fr.fakenamegenerator.com/images/sil-female.png' alt='texteAlte' />
+          <Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' alt='texteAlte' />
           <Card.Meta>Texte température (c'est froid, pluivieux...)</Card.Meta>
           <Card.Description className='whatWear'>
             Don't forget to wear ... clothes :
           </Card.Description>
-          
+          <GridExampleGrid />
         </Card.Content>
       </Card>
     );
