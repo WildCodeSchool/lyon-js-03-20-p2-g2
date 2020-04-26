ecd0c4625e6663adcbf5f3c6611a61bd1e3f97a3
Sora952 <sora952c16b10@gmail.com>
2020-04-26 12:41:17 +0200

    #10 Refonte

--- src/components/weather/cloudy.js
+++ src/components/weather/cloudy.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
-import { Card, Image, Grid } from 'semantic-ui-react';
-import _ from 'lodash';
+import { Image } from 'semantic-ui-react';
 import '../../style/cardsWeatherWears.css';
 
-const columns = _.times(5, (i) => (
-  <Grid.Column key={i}>
-    <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-  </Grid.Column>
-));
-
-const GridExampleGrid = () => <Grid>{columns}</Grid>;
+const src = 'https://react.semantic-ui.com/images/wireframe/image.png';
 
 class Cloudy extends Component {
   render () {
     return (
-      <Card className='WeatherWear'>
-        <Card.Content>
+      <div className='cloudy'>
+        <div className='WeatherClothes'>
 
-          <Card.Header>Our suggestion</Card.Header>
-          <Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' alt='texteAlte' />
-          <Card.Meta>Texte température (c'est froid, pluivieux...)</Card.Meta>
-          <Card.Description className='whatWear'>
-            Don't forget to wear ... clothes :
-          </Card.Description>
-          <GridExampleGrid />
-        </Card.Content>
-      </Card>
+          <h1>Our suggestions</h1>
+          <img className='humanoid' src={src} alt='texteAlte' />
+          <h2>Texte température (c'est froid, pluivieux...)</h2>
+          <h4>
+            Do not forget to wear ... clothes :
+          </h4>
+          <div className='clothes'>
+            <Image src={src} />
+            <Image src={src} />
+            <Image src={src} />
+            <Image src={src} />
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 export default Cloudy;
+
+{/* <Card className='WeatherWear'>
+<Card.Content>
+
+  <Card.Header>Our suggestion</Card.Header>
+  <img size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' alt='texteAlte' />
+  <Card.Meta>Texte température (c'est froid, pluivieux...)</Card.Meta>
+  <Card.Description className='whatWear'>
+    Don't forget to wear ... clothes :
+  </Card.Description>
+  <Truc />
+</Card.Content>
+</Card>
+);
+} */}
