31d6a69f18bea6211ce1dfb9932cff2d4feecb32
wdwcs <Arminit3@gmail.com>
2020-04-27 22:46:34 +0200

    #11 fix ModalSuggestions display problem

--- src/components/ModalSuggestions.js
+++ src/components/ModalSuggestions.js
@@ -10,20 +10,18 @@ const SmileysPopover = (props) => {
   return (
     <div className='smileys-popover'>
       <div>
-        <span className={clickedSmiley === 'angry' ? 'icon-clicked' : ''} onClick={() => setClickedSmiley('angry')}><span onClick={clickedSmiley === 'angry' ? () => {} : props.onClick}><i className='far fa-angry iconsPopover' /></span></span>
+        <span className={clickedSmiley === 'angry' ? 'icon-clicked' : ''} onClick={() => setClickedSmiley('angry')}><span onClick={props.onClick}><i className='far fa-angry iconsPopover' /></span></span>
         <span className={clickedSmiley === 'sad' ? 'icon-clicked' : ''} onClick={() => setClickedSmiley('sad')}><span onClick={props.onClick}><i className='far fa-frown iconsPopover' /></span></span>
         <span className={clickedSmiley === 'meh' ? 'icon-clicked' : ''} onClick={() => setClickedSmiley('meh')}><span onClick={props.onClick}><i className='far fa-meh iconsPopover' /></span></span>
         <span className={clickedSmiley === 'smile' ? 'icon-clicked' : ''} onClick={() => setClickedSmiley('smile')}><span onClick={props.onClick}><i className='far fa-smile iconsPopover' /></span></span>
         <span className={clickedSmiley === 'loving' ? 'icon-clicked' : ''} onClick={() => setClickedSmiley('loving')}><span onClick={props.onClick}><i className='far fa-grin-hearts iconsPopover' /></span></span>
       </div>
-      {props.openTextArea
-        ? // eslint-disable-line
-        <form className='text-area' name='suggestions' method='post'> { /* eslint-disable-line */ }
-          <input type='hidden' name='form-name' value='suggestions' /> { /* eslint-disable-line */ }
-          <input type="hidden" name='smiley' value={clickedSmiley}/> { /* eslint-disable-line */ }
-          <textarea className='input-textarea' placeholder='Tell us about your experience...' id='name' name='message' rows='6' cols='30' /> { /* eslint-disable-line */ }
-          <span className='close-textArea'><input type='submit' className='input-textarea' value='Send !' /></span> { /* eslint-disable-line */ }
-        </form> : ''} { /* eslint-disable-line */ }
+      <form className='text-area' name='suggestions' method='post'>
+        <input type='hidden' name='form-name' value='suggestions' />
+        <input type="hidden" name='smiley' value={clickedSmiley}/>
+        <textarea className='input-textarea' placeholder='Tell us about your experience...' id='name' name='message' rows='6' cols='30' />
+        <span className='close-textArea'><input type='submit' className='input-textarea' value='Send !' /></span>
+      </form>
     </div>
   );
 };
