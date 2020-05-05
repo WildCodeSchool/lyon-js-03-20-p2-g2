305bc1a0d1b31e31bff7d0701fb2d00cf1022e06
wdwcs <Arminit3@gmail.com>
2020-05-06 01:37:04 +0200

    #8 refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import SearchBar from './components/SearchBar';
-import About from './components/About';
-import Home from './components/Home';
-import 'weather-icons/css/weather-icons.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import SearchBar from './components/SearchBar'
+import About from './components/About'
+import Home from './components/Home'
+import 'weather-icons/css/weather-icons.css'
 
 class App extends React.Component {
   render () {
@@ -17,21 +13,19 @@ class App extends React.Component {
         <main id='App'>
           <Switch>
             <Route exact path='/'>
-              <Home />
+              <Home/>
             </Route>
-            <Route path='/favorites' />
             <Route path='/about'>
-              <About />
+              <About/>
             </Route>
-            <Route path='/parameters' />
             <Route path='/:city' component={About}>
-              <SearchBar />
+              <SearchBar/>
             </Route>
           </Switch>
         </main>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
