28662f9026a70cacc267db8f1da5d2f499aaf49d
wdwcs <Arminit3@gmail.com>
2020-05-06 01:39:38 +0200

    #10 refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import About from './components/About';
 import Home from './components/Home';
@@ -16,16 +11,13 @@ class App extends React.Component {
     return (
       <Router>
         <main id='App'>
-
           <Switch>
             <Route exact path='/'>
               <Home />
             </Route>
-            <Route path='/favorites' />
             <Route path='/about'>
               <About />
             </Route>
-            <Route path='/parameters' />
             <Route path='/:city' component={About}>
               <SearchBar />
             </Route>
