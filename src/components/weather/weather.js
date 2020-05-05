d90a12251b12dceff700390485d26192dc10473e
Sora952 <sora952c16b10@gmail.com>
2020-05-05 14:04:40 +0200

    #10 modif

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -11,28 +11,28 @@ let clothes = '';
 5: "Rain"
 7: "Clear"
 8: "Clouds"
-"*/
+" */
 class Weather extends Component {
-  render() {
+  render () {
     const temperature = this.props.min;
     const description = this.props.main;
 
     if (description.includes('Snow')) {
       clothes = <Snowy />;
     } else if ((temperature >= 3 && temperature <= 16) && description.includes('Clouds')) {
-        clothes = <Cold />;
+      clothes = <Cold />;
     } else if (temperature < 3 && description.includs('Sunny')) {
-        clothes = <Cold />;
+      clothes = <Cold />;
     } else if (temperature < 3 && description.includes('Clouds')) {
-        clothes = <Cold />;
+      clothes = <Cold />;
     } else if ((temperature > 17 && temperature <= 38) && description.includes('Sunny')) {
-        clothes = <Sunny />;
-    } else if ((temperature > 17 && temperature <=38) && description.includes('Clouds')) {
-        clothes = <Sunny />;
+      clothes = <Sunny />;
+    } else if ((temperature > 17 && temperature <= 38) && description.includes('Clouds')) {
+      clothes = <Sunny />;
     } else if (temperature >= 39) {
       clothes = <VeryHot />;
     } else if (description.includes('Rain')) {
-        clothes = <Rainy />;
+      clothes = <Rainy />;
     }
 
     return (
