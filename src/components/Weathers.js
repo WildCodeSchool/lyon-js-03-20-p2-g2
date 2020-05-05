d2b5bd191e96ca44a0323646354d1a1480718437
wdwcs <Arminit3@gmail.com>
2020-05-05 20:51:34 +0200

    #10 update

--- src/components/Weathers.js
+++ src/components/Weathers.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Weather from './Weather';
 
 const Rainy = {
-  img: require('../images/gif/tenor.gif'),
+  img: require('../images/gif/rainy1.gif'),
   p: 'The weather will be cloudy and overcast, so don\'t forget to cover yourself, a jacket, some boots will be useful and don\'t forget your umbrella',
   img1: require('../images/rain-boots.png'),
   img2: require('../images/umbrella.png'),
@@ -10,12 +10,12 @@ const Rainy = {
   img4: ''
 };
 const Cold = {
-  img: require('../images/gif/link-cold.gif'),
+  img: require('../images/gif/cold1.gif'),
   p: 'The weather will be cloudy and overcast, so don\'t forget to cover yourself, a jacket, a hat and a scarf will be useful',
   img1: require('../images/bonnet.png'),
   img2: require('../images/coat.png'),
-  img3: require('../images/scarf.png'),
-  img4: require('../images/gloves.png')
+  img3: require('../images/gloves.png'),
+  img4: require('../images/scarf.png')
 };
 const Sunny = {
   img: require('../images/image-sunny.png'),
@@ -26,7 +26,7 @@ const Sunny = {
   img4: require('../images/sunglasses.png')
 };
 const Snowy = {
-  img: require('../images/image-snowy.png'),
+  img: require('../images/gif/snowy1.gif'),
   p: 'The weather will be snowy and cold, so don\'t forget to cover yourself with a jacket, a hat and a scarf.Let it snow!',
   img1: require('../images/snow-boots.png'),
   img2: require('../images/gloves.png'),
@@ -34,7 +34,7 @@ const Snowy = {
   img4: require('../images/coat.png')
 };
 const Temperate = {
-  img: require('../images/image-cold.png'),
+  img: require('../images/gif/temperate1.gif'),
   p: 'The temperature is moderate today, it is the good moment to go outside if you want to take a walk or practice some sports !\n' +
     '  Feel free to wear comfortable clothes !',
   img1: require('../images/tshirt.png'),
@@ -43,7 +43,7 @@ const Temperate = {
   img4: require('../images/dress.png')
 };
 const VeryHot = {
-  img: require('../images/image-cold.png'),
+  img: require('../images/gif/hot1.gif'),
   p: 'It is very hot today, you should go out only if you need to.\n' +
     '          You must drink a lot of water, cover your head and find cool places if you go outside.',
   img1: require('../images/bottle.png'),
@@ -59,32 +59,20 @@ class Weathers extends Component {
     const temperature = this.props.min;
     const description = this.props.main;
 
-    if (temperature < 3 && description.includes('Snow')) {
-      clothes = <Weather {...Snowy} />;
+    if (description.includes('Rain')) {
+      clothes = <Weather {...Rainy} />;
     } else if (description.includes('Snow')) {
       clothes = <Weather {...Snowy} />;
-    } else if ((temperature >= 3 && temperature <= 7) && description.includes('Clouds')) {
+    } else if (temperature <= 10) {
       clothes = <Weather {...Cold} />;
-    } else if (temperature < 3 && description.includes('Sunny')) {
-      clothes = <Weather {...Cold} />;
-    } else if (temperature < 3 && description.includes('Clouds')) {
-      clothes = <Weather {...Cold} />;
-    } else if ((temperature > 18 && temperature <= 30) && description.includes('Sunny')) {
+    } else if ((temperature >= 10 && temperature <= 18)) {
+      clothes = <Weather {...Temperate} />;
+    } else if ((temperature > 18 && temperature <= 30 && description.includes('Clouds'))) {
       clothes = <Weather {...Sunny} />;
-    } else if ((temperature > 18 && temperature <= 30) && description.includes('Clouds')) {
-      clothes = <Weather {...Sunny} />;
-    } else if ((temperature >= 8 && temperature <= 18) && description.includes('Clear')) {
-      clothes = <Weather {...Temperate} />;
-    } else if ((temperature >= 8 && temperature <= 18) && description.includes('Clouds')) {
-      clothes = <Weather {...Temperate} />;
-    } else if ((temperature >= 8 && temperature <= 18) && description.includes('Sunny')) {
-      clothes = <Weather {...Temperate} />;
     } else if (temperature > 18 && temperature <= 30) {
-      clothes = <Weather {...Sunny} />;
+      clothes = <Weather {...Temperate} />;
     } else if (temperature >= 31) {
       clothes = <Weather {...VeryHot} />;
-    } else if (description.includes('Rain')) {
-      clothes = <Weather {...Rainy} />;
     } else clothes = <h2 align='center'>no data</h2>;
 
     return (
