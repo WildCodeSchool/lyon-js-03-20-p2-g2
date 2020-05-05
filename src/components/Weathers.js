3018f6f652a9fbc4d36932e9d5bc734265d2c125
wdwcs <Arminit3@gmail.com>
2020-05-06 01:13:34 +0200

    #10 refactoring

--- src/components/Weathers.js
+++ src/components/Weathers.js
@@ -2,59 +2,59 @@ import React, { Component } from 'react';
 import Weather from './Weather';
 
 const Rainy = {
-  img: require('../images/gif/rainy1.gif'),
+  img: require('../images/suggestions/rainy1.gif'),
   p: 'The weather will be cloudy and overcast, so don\'t forget to cover yourself, a jacket, some boots will be useful and don\'t forget your umbrella',
-  img1: require('../images/rain-boots.png'),
-  img2: require('../images/umbrella.png'),
-  img3: require('../images/rain-coat.png'),
-  img4: require('../images/bonnet.png'),
+  img1: require('../images/suggestions/rain-boots.png'),
+  img2: require('../images/suggestions/umbrella.png'),
+  img3: require('../images/suggestions/rain-coat.png'),
+  img4: require('../images/suggestions/bonnet.png'),
   alt: 'rainy-weather'
 };
 const Cold = {
-  img: require('../images/gif/cold1.gif'),
+  img: require('../images/suggestions/cold1.gif'),
   p: 'The weather will be cloudy and overcast, so don\'t forget to cover yourself, a jacket, a hat and a scarf will be useful',
-  img1: require('../images/bonnet.png'),
-  img2: require('../images/coat.png'),
-  img3: require('../images/gloves.png'),
-  img4: require('../images/scarf.png'),
+  img1: require('../images/suggestions/bonnet.png'),
+  img2: require('../images/suggestions/coat.png'),
+  img3: require('../images/suggestions/gloves.png'),
+  img4: require('../images/suggestions/scarf.png'),
   alt: 'cold-weather'
 };
 const Sunny = {
-  img: require('../images/image-sunny.png'),
+  img: require('../images/suggestions/image-sunny.png'),
   p: 'The weather will be sunny so don\'t cover yourself too much but protect you skin and don\'t forget your glasses',
-  img1: require('../images/tee-shirt.png'),
-  img2: require('../images/casquette.png'),
-  img3: require('../images/sunscreen.png'),
-  img4: require('../images/sunglasses.png'),
+  img1: require('../images/suggestions/tee-shirt.png'),
+  img2: require('../images/suggestions/casquette.png'),
+  img3: require('../images/suggestions/sunscreen.png'),
+  img4: require('../images/suggestions/sunglasses.png'),
   alt: 'sunny-weather'
 };
 const Snowy = {
-  img: require('../images/gif/snowy1.gif'),
+  img: require('../images/suggestions/snowy1.gif'),
   p: 'The weather will be snowy and cold, so don\'t forget to cover yourself with a jacket, a hat and a scarf.Let it snow!',
-  img1: require('../images/snow-boots.png'),
-  img2: require('../images/gloves.png'),
-  img3: require('../images/bonnet.png'),
-  img4: require('../images/coat.png'),
+  img1: require('../images/suggestions/snow-boots.png'),
+  img2: require('../images/suggestions/gloves.png'),
+  img3: require('../images/suggestions/bonnet.png'),
+  img4: require('../images/suggestions/coat.png'),
   alt: 'snowy-weather'
 };
 const Temperate = {
-  img: require('../images/gif/temperate1.gif'),
+  img: require('../images/suggestions/temperate1.gif'),
   p: 'The temperature is moderate today, it is the good moment to go outside if you want to take a walk or practice some sports !\n' +
     '  Feel free to wear comfortable clothes !',
-  img1: require('../images/tshirt.png'),
-  img2: require('../images/jean.png'),
-  img3: require('../images/sportshoes.png'),
-  img4: require('../images/dress.png'),
+  img1: require('../images/suggestions/tshirt.png'),
+  img2: require('../images/suggestions/jean.png'),
+  img3: require('../images/suggestions/sportshoes.png'),
+  img4: require('../images/suggestions/dress.png'),
   alt: 'temperate-weather'
 };
 const VeryHot = {
-  img: require('../images/gif/hot1.gif'),
+  img: require('../images/suggestions/hot1.gif'),
   p: 'It is very hot today, you should go out only if you need to.\n' +
     '          You must drink a lot of water, cover your head and find cool places if you go outside.',
-  img1: require('../images/bottle.png'),
-  img2: require('../images/ventilator.png'),
-  img3: require('../images/swimsuit.png'),
-  img4: require('../images/sunscreen.png'),
+  img1: require('../images/suggestions/bottle.png'),
+  img2: require('../images/suggestions/ventilator.png'),
+  img3: require('../images/suggestions/swimsuit.png'),
+  img4: require('../images/suggestions/sunscreen.png'),
   alt: 'very-hot-weather'
 };
 
