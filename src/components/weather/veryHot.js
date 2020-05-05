2b82067ddc9888eb3f5e2e3a83c7d42f0d2180b3
Sora952 <sora952c16b10@gmail.com>
2020-05-05 13:59:46 +0200

    #10 modif

---
+++ src/components/weather/veryHot.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Image } from 'semantic-ui-react';
+import '../../style/cardsWeatherWears.css';
+
+class VeryHot extends Component {
+  render () {
+    return (
+      <div className='veryHot'>
+        <div className='weatherCard'>
+          <h1 className='ourSuggest'>Our suggestions</h1>
+          <img className='humanoid' src={require('../../images/image-cold.png')} alt='clothingHuman' />
+          <h2 className='textClothes'>Il fait chaud</h2>
+          <h4 className='dontForget'>Do not forget to wear ...</h4>
+          <div className='clothes'>
+            <Image src={require('../../images/bonnet.jpg')} />
+            <Image src={require('../../images/coat.jpg')} />
+            <Image src={require('../../images/gloves.jpg')} />
+            <Image src={require('../../images/scarf.jpg')} />
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default VeryHot;
--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -3,6 +3,7 @@ import Cold from './cold';
 import Snowy from './snowy';
 import Sunny from './sunny';
 import Rainy from './rain';
+import VeryHot from './veryHot';
 
 let clothes = '';
 /* "Clouds"
@@ -18,12 +19,20 @@ class Weather extends Component {
 
     if (description.includes('Snow')) {
       clothes = <Snowy />;
-    } else if (((temperature >= 3 && temperature <= 16) && description.includes('Clouds')) || (temperature < 3 && description.includs('Sunny')) {
-      clothes = <Cold />;
-    } else if ((temperature > 17 && description.includes('Sunny')) || (temperature > 17 && description.includes('Clouds'))) {
-      clothes = <Sunny />;
+    } else if ((temperature >= 3 && temperature <= 16) && description.includes('Clouds')) {
+        clothes = <Cold />;
+    } else if (temperature < 3 && description.includs('Sunny')) {
+        clothes = <Cold />;
+    } else if (temperature < 3 && description.includes('Clouds')) {
+        clothes = <Cold />;
+    } else if ((temperature > 17 && temperature <= 38) && description.includes('Sunny')) {
+        clothes = <Sunny />;
+    } else if ((temperature > 17 && temperature <=38) && description.includes('Clouds')) {
+        clothes = <Sunny />;
+    } else if (temperature >= 39) {
+      clothes = <VeryHot />;
     } else if (description.includes('Rain')) {
-      clothes = <Rainy />;
+        clothes = <Rainy />;
     }
 
     return (
