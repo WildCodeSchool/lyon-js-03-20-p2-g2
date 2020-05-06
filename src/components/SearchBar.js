b54c6605c103f9f40c90cf3bab5eb3a51bfa3379
abdel <a.akroh@e-durable.ch>
2020-05-06 17:53:36 +0200

    aqi conflict

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -142,7 +142,7 @@ class SearchBar extends React.Component {
     if (event.key === 'Enter') {
       event.preventDefault();
       const city = event.target.value;
-      this.setState({ city: city, meteoByGeo: false });
+      this.setState({ city: city, meteoByGeo: false, AQI: null });
       this.fetchOnClick(city);
     }
   }
