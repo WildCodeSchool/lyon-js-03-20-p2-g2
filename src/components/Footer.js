5b53677a8a1ca567e82261c5468cc66c44ceceb9
abdel <a.akroh@e-durable.ch>
2020-05-09 23:03:52 +0200

    ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 import ModalSuggestions from './ModalSuggestions';
 
 class Footer extends Component {
-  render () {
+  render() {
     return (
       <footer className='page-footer font-small blue'>
         <Link className='link' to='/about'>About us</Link>
-        <div className='footer-copyright text-center py-3 footer-style'>© 2020<Link className='' to='/'> Weather Suggest</Link></div>
+        <div className='footer-copyright text-center py-3 footer-style'>© 2020<Link className='' to='/'> Weather Suggest <i className='fas fa-heart coeur' /></Link></div>
         <ModalSuggestions />
       </footer>
     );
