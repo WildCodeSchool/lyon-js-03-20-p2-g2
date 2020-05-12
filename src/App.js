649c71fe58e10e920e46d9e6dc49888055649055
wdwcs <Arminit3@gmail.com>
2020-05-12 22:38:32 +0200

    #9 refactoring

--- src/App.js
+++ src/App.js
@@ -15,12 +15,9 @@ const Background = styled.div`
     (time >= 12 && time < 18 && 'url("https://img-weather.netlify.app/background/background_day3.jpg")') ||
     (time >= 18 && time <= 23 && 'url("https://img-weather.netlify.app/background/background_dusk3.jpg")') ||
     (time >= 0 && time < 6 && 'url("https://img-weather.netlify.app/background/background_night7.jpg")')
-  };
+  } no-repeat fixed center;
     min-height: 100vh;
-    background-size: cover;
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;`;
+    background-size: cover;`;
 
 class App extends React.Component {
   render () {
@@ -30,7 +27,6 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/'>
               <Background>
-
                 <Header />
                 <SearchBar />
                 <Footer />
