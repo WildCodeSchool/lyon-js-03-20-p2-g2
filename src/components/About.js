2f567732faa4c2c06fb4b49d6db4d79575f27759
abdel <a.akroh@e-durable.ch>
2020-05-13 08:50:20 +0200

    #9 remove outline btn back

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 class About extends Component {
-  render() {
+  render () {
     return (
       <>
         <section className='about'>
