502ac76cc77d376aca7c69b40933ea4ea6b2a070
abdel <a.akroh@e-durable.ch>
2020-05-14 16:21:28 +0200

    #9 Add setime out

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -20,7 +20,7 @@ const keyAQI = 'a21a5dc572269b362928535f3857be9975516906';
 const keyDarkSky = 'cfeed98eb60dc557187a9ea2c357cd52';
 
 class SearchBar extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       up: true,
@@ -61,7 +61,7 @@ class SearchBar extends React.Component {
   /* La méthode renderSuggestions me permet de mapper les villes et de proposer une liste (ul) de villes correspondant aux premiers
   caractères entrés par l'utilisateur. Au clic sur l'un des choix de ville, j'appelle ensuite handleSuggestionSelected. */
 
-  renderSuggestions () {
+  renderSuggestions() {
     const { suggestions } = this.state;
     if (suggestions.length === 0) {
       return null;
@@ -78,7 +78,7 @@ class SearchBar extends React.Component {
     Grâce à cette fonction, j'appelle ensuite fetchOnClik qui prend en paramètre 'text' de mon state qui a été updatée avec le click
     de l'utilisateur. */
 
-  async handleSuggestionSelected (value) {
+  async handleSuggestionSelected(value) {
     await this.setState(() => ({
       text: value,
       suggestions: [],
@@ -91,7 +91,7 @@ class SearchBar extends React.Component {
     Elle prend en paramètre la ville choisie (cliquée) par l'utilisateur et, grâce à cette ville, on va aller chercher la météo correspondante.
     Lorsque l'on a la météo de la ville, on remplace les données de notre propriété meteoBySearch (du state) par les données recueillies par l'API. */
 
-  async fetchOnClick (city) {
+  async fetchOnClick(city) {
     const { favorites } = this.state;
 
     const searchCityUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${Apikeyw}`;
@@ -171,7 +171,7 @@ class SearchBar extends React.Component {
     A ce moment là, je change la 'city' de mon state avec la valeur qu'a entré mon utilisateur.
     J'appelle ensuite fetchSearchResults qui va prendre en paramètre 'city'. */
 
-  handleChange (event, city) {
+  handleChange(event, city) {
     if (event.key === 'Enter') {
       event.preventDefault();
       const city = event.target.value;
@@ -184,89 +184,92 @@ class SearchBar extends React.Component {
   /* La méthode handleClick va fonctionner la même façon que fetchOnClick mais au click cette fois.
     Elles va recueillir les coordonnées de l'utilisateur (getCurrentPosition) pour ensuite afficher les données de la météo. */
 
-  async handleClick (e) {
+  async handleClick(e) {
     e.preventDefault();
     const { favorites } = this.state;
     this.setState({ loading: true });
-    this.setState({ weatherForecast: false, AQI: null, alerts: null, Pollution: null });
+    setTimeout(() => {
 
-    navigator.geolocation.getCurrentPosition(pos => {
-      this.setState({ loading: false },
-        () => {
-          this.setState({ lat: parseFloat(pos.coords.latitude.toFixed(3)), long: parseFloat(pos.coords.longitude.toFixed(3)) });
-        }
-      );
+      this.setState({ weatherForecast: false, AQI: null, alerts: null, Pollution: null });
 
-      const searchCityUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.lat}&lon=${this.state.long}&appid=${Apikeyw}`;
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.setState({ loading: false },
+          () => {
+            this.setState({ lat: parseFloat(pos.coords.latitude.toFixed(3)), long: parseFloat(pos.coords.longitude.toFixed(3)) });
+          }
+        );
 
-      axios.get(searchCityUrl)
-        .then(res => res.data)
-        .then(data => {
-          this.setState({
+        const searchCityUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.lat}&lon=${this.state.long}&appid=${Apikeyw}`;
 
-            weatherForecast: {
-              city: data.city.name.replace('Arrondissement de', ''),
-              country: data.city.country,
-              sunrise: data.city.sunrise,
-              sunset: data.city.sunset,
-              temperature: Math.round(data.list[0].main.temp - 273.15),
-              feelslike: Math.round(data.list[0].main.feels_like - 273.15),
-              tempmin: Math.round(data.list[0].main.temp_min - 273.15),
-              tempmax: Math.round(data.list[0].main.temp_max - 273.15),
-              pressure: data.list[0].main.pressure,
-              humidity: data.list[0].main.humidity,
-              wind: data.list[0].wind.speed,
-              icon: data.list[0].weather[0].icon,
-              weatherData: data.list,
-              main: data.list[0].weather[0].main,
-              lat: data.city.coord.lat,
-              lon: data.city.coord.lon
+        axios.get(searchCityUrl)
+          .then(res => res.data)
+          .then(data => {
+            this.setState({
+
+              weatherForecast: {
+                city: data.city.name.replace('Arrondissement de', ''),
+                country: data.city.country,
+                sunrise: data.city.sunrise,
+                sunset: data.city.sunset,
+                temperature: Math.round(data.list[0].main.temp - 273.15),
+                feelslike: Math.round(data.list[0].main.feels_like - 273.15),
+                tempmin: Math.round(data.list[0].main.temp_min - 273.15),
+                tempmax: Math.round(data.list[0].main.temp_max - 273.15),
+                pressure: data.list[0].main.pressure,
+                humidity: data.list[0].main.humidity,
+                wind: data.list[0].wind.speed,
+                icon: data.list[0].weather[0].icon,
+                weatherData: data.list,
+                main: data.list[0].weather[0].main,
+                lat: data.city.coord.lat,
+                lon: data.city.coord.lon
+              },
+
+              loading: false,
+              errorMessage: false,
+              up: false
+
             },
+              () => this.setState({ text: data.city.name.replace('Arrondissement de', '') },
+                () => {
+                  if (!favorites.some(alreadyFavorite => alreadyFavorite.toLowerCase() === this.state.text.toLowerCase())) {
+                    this.setState({ liked: null });
+                  } else {
+                    this.setState({ liked: 'yes' });
+                  }
+                }, () => this.setState({ up: false })),
 
-            loading: false,
-            errorMessage: false,
-            up: false
-
-          },
-          () => this.setState({ text: data.city.name.replace('Arrondissement de', '') }, () => this.setState({ up: false }),
-            () => {
-              if (!favorites.some(alreadyFavorite => alreadyFavorite.toLowerCase() === this.state.text.toLowerCase())) {
-                this.setState({ liked: null });
-              } else {
-                this.setState({ liked: 'yes' });
-              }
-            }, () => this.setState({ up: false })),
-
-          this.fetchAlertsData(this.state.lat, this.state.lon)
-          );
-        })
-        .catch(error => { /* eslint-disable-line */
-          console.log('Please search again...');
-          this.setState({ errorMessage: true });
-        });
-
-      axios.get(`https://api.waqi.info/feed/geo:${this.state.lat};${this.state.long}/?token=${keyAQI}`)
-        .then(res => res.data)
-        .then(data => {
-          this.setState({
-            AQI: data.data.aqi,
-            pollutionIndex: {
-              NO2: data.data.iaqi.no2.v,
-              O3: data.data.iaqi.o3.v,
-              PM10: data.data.iaqi.pm10.v
-            }
-
+              this.fetchAlertsData(this.state.lat, this.state.lon)
+            );
+          })
+          .catch(error => { /* eslint-disable-line */
+            console.log('Please search again...');
+            this.setState({ errorMessage: true });
           });
-        })
 
-        .catch(error => {   /* eslint-disable-line */
-          console.log('Please search again...');
-          this.setState({ errorMessage: true });
-        });
-    });
+        axios.get(`https://api.waqi.info/feed/geo:${this.state.lat};${this.state.long}/?token=${keyAQI}`)
+          .then(res => res.data)
+          .then(data => {
+            this.setState({
+              AQI: data.data.aqi,
+              pollutionIndex: {
+                NO2: data.data.iaqi.no2.v,
+                O3: data.data.iaqi.o3.v,
+                PM10: data.data.iaqi.pm10.v
+              }
+
+            });
+          })
+
+          .catch(error => {   /* eslint-disable-line */
+            console.log('Please search again...');
+            this.setState({ errorMessage: true });
+          });
+      });
+    }, 1000);
   }
 
-  async fetchAlertsData (lat, long) {
+  async fetchAlertsData(lat, long) {
     await axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${keyDarkSky}/${this.state.lat},${this.state.long}`)
       .then(res => res.data)
       .then(data => {
@@ -281,9 +284,12 @@ class SearchBar extends React.Component {
             }
           });
       });
+
+
   }
 
-  unixTimestamp (t) {
+
+  unixTimestamp(t) {
     const dt = new Date(t * 1000);
     const hr = dt.getHours();
     const m = '0' + dt.getMinutes();
@@ -315,13 +321,13 @@ class SearchBar extends React.Component {
     );
   }
 
-  componentDidMount () {
+  componentDidMount() {
     localStorage.getItem('favorites') /* eslint-disable-line */
       ? this.setState({ favorites: JSON.parse(localStorage.getItem('favorites')) }) /* eslint-disable-line */
       : localStorage.setItem('favorites', JSON.stringify(this.state.favorites)); /* eslint-disable-line */
   }
 
-  componentDidUpdate (prevState) {
+  componentDidUpdate(prevState) {
     localStorage.setItem('favorites', JSON.stringify(this.state.favorites)); /* eslint-disable-line */
   }
 
@@ -329,7 +335,7 @@ class SearchBar extends React.Component {
     event.target.select();
   }
 
-  render () {
+  render() {
     const { loading, favorites, weatherForecast, liked, temp, AQI, pollutionIndex, errorMessage, alerts, up } = this.state;
 
     return (
@@ -363,7 +369,7 @@ class SearchBar extends React.Component {
               </p>
             </div>}
 
-          {loading && <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '10px' }}><CircularProgress style={{ width: '100px', height: '100px' }} /></div>}
+          {loading && <div style={{ display: 'flex', justifyContent: 'center' }}><CircularProgress style={{ width: '100px', height: '100px' }} /></div>}
 
           {favorites &&
             <ul className='list-favorites'>{favorites.map((favorite, index) => <li style={{ cursor: 'pointer' }} key={index}><span onClick={() => this.fetchOnClick(favorite)} className='city-favorite'>{favorite}</span> <span onClick={() => this.deleteFavorite({ favorite })}><i className='fas fa-times deleting-city' /></span></li>)}</ul>}
