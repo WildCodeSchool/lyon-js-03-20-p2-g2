5074fdb7aea3c927e8bd4f84c3a00f7afb5eb1e4
Sora952 <59966470+Sora952@users.noreply.github.com>
2020-08-23 15:28:20 +0200

    Update img background

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ const today = new Date();
 const Background = styled.div`
     background: ${({ time = today.getHours() }) =>
 
-    (time >= 6 && time < 12 && 'url("https://img-weather.netlify.app/background/background_dawn2.jpg")') ||
-    (time >= 12 && time < 18 && 'url("https://img-weather.netlify.app/background/background_day3.jpg")') ||
+    (time >= 6 && time < 12 && 'url("./images/background/background_dawn2.jpg")') ||
+    (time >= 12 && time < 18 && 'url("./images/background/background_day3.jpg")') ||
     (time >= 18 && time <= 23 && 'url("https://img-weather.netlify.app/background/background_dusk3.jpg")') ||
-    (time >= 0 && time < 6 && 'url("https://img-weather.netlify.app/background/background_night7.jpg")')
+    (time >= 0 && time < 6 && 'url("./images/background/background_night7.jpg")')
   } no-repeat fixed center;
     min-height: 100vh;
     background-size: cover;`;
